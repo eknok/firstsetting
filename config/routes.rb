3848fa8cfc50aed07b339935748d59e59fee314d
EikiKanaoka <kakakaeee@gmail.com>
2018-07-20 19:45:27 +0900

    日本語化 Timezone to Japanese Add config/locales/ja.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+    get 'login',  to: 'devise/sessions#new'
+  end
+
   root to: 'toppages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
